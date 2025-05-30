2f7da1da40041328ad2aca51e9523553e1861353
Anish gehlot <anishgehlot@gmail.com>
2025-05-30 11:27:18 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
-module "modules" {
-  source  = "app.terraform.io/anishgehlot/modules/storage"
+module "log_analytics_workspace" {
+  source  = "app.terraform.io/anishgehlot/log_analytics_workspace/azure"
   version = "1.0.0"
+
+  workspace_name      = "laws"
+  workspace_retention = 30
+  sku                 = PerGB2018
+  tags                = {
+    evn = dev
+  }
 
 }
